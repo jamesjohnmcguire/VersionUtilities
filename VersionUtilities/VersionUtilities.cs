db5c3da888b0c402e2c614ee5b894a0594629238
JamesMc <jamesjohnmcguire@gmail.com>
2022-01-04 14:22:35 +0900

    Apply copyright year updates

--- VersionUtilities/VersionUtilities.cs
+++ VersionUtilities/VersionUtilities.cs
@@ -1,6 +1,6 @@
 ﻿/////////////////////////////////////////////////////////////////////////////
 // <copyright file="VersionUtilities.cs" company="James John McGuire">
-// Copyright © 2006 - 2021 James John McGuire. All Rights Reserved.
+// Copyright © 2020 - 2022 James John McGuire. All Rights Reserved.
 // </copyright>
 /////////////////////////////////////////////////////////////////////////////
 
